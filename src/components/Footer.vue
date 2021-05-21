181013ecccef1627569af0b0b6fa05bb0dc4529c
BadBoyFromUA <tankist-wot1@ukr.net>
2021-05-21 20:56:51 +0300

    lab5

---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+    <footer>
+        <p id="footer_text">Made by Ostap Semchyshyn</p>
+    </footer>
+</template>
+
+<script>
+    export default {
+        name: "Footer"
+    }
+</script>
+
+<style scoped>
+
+</style>
